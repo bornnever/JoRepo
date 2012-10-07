d4d2b122bdb4e3d8c101f74010ea803a0ae375ac
JothishNair <jothishrnair@gmail.com>
2012-10-07 12:48:10 -0400

    New commit

---
+++ WinC_Test/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinC_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime _CurrentTime = DateTime.Now;
+            Boolean s;
+            s= TimerDayAndTimeCheck("Thursday","0","23");
+
+        }
+
+         private Boolean TimerDayAndTimeCheck (String SettingNameDaysofWeek, String SettingNameHours,String SettingDays)
+            {
+
+            DateTime _CurrentTime = DateTime.Now;
+            String strDaysofWeek, strHours,strDays;
+            try
+                {
+                strDaysofWeek = SettingNameDaysofWeek;
+                strHours = SettingNameHours;
+                strDays = SettingDays;
+                Boolean k;
+                k = strHours.Split(',').Select(i => Convert.ToString(i)).ToList().Contains(_CurrentTime.Hour.ToString());
+                if ((strDaysofWeek.Split(',').Select(i => Convert.ToString(i).ToUpper()).ToList().Contains(_CurrentTime.DayOfWeek.ToString().ToUpper()) == true)  &&
+                       (strHours.Split(',').Select(i => Convert.ToString(i)).ToList().Contains(_CurrentTime.Hour.ToString()) == true) &&
+                    (strDays.Split(',').Select(i => Convert.ToString(i)).ToList().Contains(_CurrentTime.Day.ToString()) == true)
+                    )
+                    {
+                    return true;
+                    }
+                else
+                    {
+                    return false;
+                    }
+                }
+            catch (Exception e)
+            { throw e; }
+            }
+        }
+         
+    }
+ 
